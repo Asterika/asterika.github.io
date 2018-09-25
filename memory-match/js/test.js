2d762e18acce9f10cdf193b1f1ace9257f4042b6
Krystal Williams <asterika@Krystals-MBP.hsd1.wa.comcast.net>
2018-09-24 18:02:28 -0700

    Created game logic - created match and noMatch functions, set time delay for card flips

--- memory-match/js/test.js
+++ memory-match/js/test.js
@@ -24,7 +24,7 @@ const cards = [
    //multiply number of cards in array by 2???
     //FOR NOW, simply add a copy of each card to test randomization
  //create an array to store flipped cards (to check for match)
-const cardsFlipped = [];
+let cardsFlipped = [];
 //  //for testing, create a variable to represent object1 in the cards array
 // let cardOne = (cards[0]);
 //  //push first card into cardsFlipped array:
@@ -37,6 +37,15 @@ const cardsFlipped = [];
 
 //PUSH ALL CARDS INTO CARDS FLIPPED ARRAY!!!!
 
+// $('.card').addEventListener('click', function (event) {
+//   let clicked = event.target;
+//
+//   let count = 0;
+//     if (count < 2) {
+//       count++;
+//       clicked.addClass('flipped');
+//   }
+// })
  //===============================
 //         FLIP 2 CARDS
 //===============================
@@ -60,41 +69,62 @@ const cardsFlipped = [];
     if($target.is('img')) {
       $img = $target;
       cardsFlipped.push($img);
-
     }
     $target.toggleClass('rotated');
     checkPair();
-    // if(cardsFlipped.length === 2) {
-    //   alert('Two card max!');
-    // }
   });
 
+
 const checkPair = () => {
-  //call $flipCard() function to ensure cards have been flipped
-  // $flipCard();
     //check that 2 cards, and only 2 cards, have been flipped
     if(cardsFlipped.length === 2) {
-        // console.log(cardsFlipped);
       //check for a match
-      console.log(cardsFlipped[0].attr('value'));
-      console.log(cardsFlipped[1].attr('value'));
+      // console.log(cardsFlipped[0].attr('value'));
+      // console.log(cardsFlipped[1].attr('value'));
       if(cardsFlipped[0].attr('value') === cardsFlipped[1].attr('value')) {
-        //ULTIMATELY...call match() function
-            //add animation
- // const currentTurn = () => {
-            //remove event listener on 2 matched cards???
-         //FOR NOW...placeholder alert
+        // console.log(cardsFlipped);
+        match();
+
+        //remove event addEventListener -- no need to remove, since .one click is there
+        // cardsFlipped[0]
+        cardsFlipped = [];
+
       } else {
-        //ULTIMATELY...call noMatch() function
-          //add animation - shake/vibrate
-           //return cards to initial state through reverseFlip() function
-         //FOR NOW...placeholder alert
+        console.log(cardsFlipped[0].parent().parent());
+        console.log(cardsFlipped[1].parent().parent());
+        //add animation -- cardsFlipped[1]
+        // cardsFlipped[0].parent().parent().removeClass('rotated');
+        window.setTimeout(function() {cardsFlipped[0].parent().parent().removeClass('rotated') }, 1200);
+        // cardsFlipped[1].parent().parent().removeClass('rotated');
+        //run .one on each of cards in array or [0]/[1]
+        //flip cards
+        //empty array
+        window.setTimeout(function() {cardsFlipped[1].parent().parent().removeClass('rotated') }, 1200);
+        // console.log(window);
       }
+    } else {
     }
  }
 
+const match = () => {
+  $(cardsFlipped[0]).parent().parent().addClass('match');
+  $(cardsFlipped[1]).parent().parent().addClass('match');
+}
 
-
+// const match = () => {
+//  let flipped = document.querySelectorAll('.flipped');
+//  flipped.forEach(card => {
+//    card.addClass('match');
+//  });
+// }
+//
+// const noMatch = () => {
+//
+// }
+//
+// const reverseFlip = () => {
+//
+// }
 
 
 //GET THE ID FOR THAT CARD AND GET IT TO LOG
@@ -122,29 +152,29 @@ const checkPair = () => {
  //==============================
 //      CHECK FOR A MATCH
 //==============================
- //create a function to check if pair of flipped cards matches
-const checkPair = () => {
-  //call $flipCard() function to ensure cards have been flipped
-  $flipCard();
-    //check that 2 cards, and only 2 cards, have been flipped
-    if(cardsFlipped.length === 2) {
-      //check for a match
-      if(cardsFlipped[0] === cardsFlipped[1]) {
-        //ULTIMATELY...call match() function
-            //add animation
- // const currentTurn = () => {
-            //remove event listener on 2 matched cards???
-         //FOR NOW...placeholder alert
-        alert('Match!!!');
-      } else {
-        //ULTIMATELY...call noMatch() function
-          //add animation - shake/vibrate
-           //return cards to initial state through reverseFlip() function
-         //FOR NOW...placeholder alert
-        alert('No match!');
-      }
-    }
- }
+//  //create a function to check if pair of flipped cards matches
+// const checkPair = () => {
+//   //call $flipCard() function to ensure cards have been flipped
+//   $flipCard();
+//     //check that 2 cards, and only 2 cards, have been flipped
+//     if(cardsFlipped.length === 2) {
+//       //check for a match
+//       if(cardsFlipped[0] === cardsFlipped[1]) {
+//         //ULTIMATELY...call match() function
+//             //add animation
+//  // const currentTurn = () => {
+//             //remove event listener on 2 matched cards???
+//          //FOR NOW...placeholder alert
+//         // alert('Match!!!');
+//       } else {
+//         //ULTIMATELY...call noMatch() function
+//           //add animation - shake/vibrate
+//            //return cards to initial state through reverseFlip() function
+//          //FOR NOW...placeholder alert
+//         alert('No match!');
+//       }
+//     }
+//  }
 
 //
 //===============================
@@ -165,7 +195,7 @@ const checkPair = () => {
 //     //container = grid or cards = grid?
 //     // yourArray.sort(function() { return 0.5 - Math.random() });
 // cards.sort(function() {return 0.5 - Math.random() });
-//     // cardArray.sort(() => 0.5 - Math.random());
+//     cards.sort(() => 0.5 - Math.random());
 //
 // //create a function that defines a 'turn'
 //     //define flip/clickOne and flip/clickTwo ($flipToBack = one click)
